c84528afd96982f16808f9e72682c203028adae8
Jan <jkaminski677@gmail.com>
2022-04-06 00:24:33 +0200

    The SHOP website has been completed. Added buttons to change the appearance of the displayed products.Links to other sites have been corrected.

--- code.js
+++ code.js
@@ -56,15 +56,16 @@
 // }
 
 // window.onload = changeImg;
+/*
 
 var faq = document.getElementsByClassName("faq-page");
 var i;
 for (i = 0; i < faq.length; i++) {
     faq[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
+        // Toggle between adding and removing the "active" class,
+        // to highlight the button that controls the panel
         this.classList.toggle("active");
-        /* Toggle between hiding and showing the active panel */
+        // /* Toggle between hiding and showing the active panel
         var body = this.nextElementSibling;
         if (body.style.display === "block") {
             body.style.display = "none";
@@ -72,4 +73,113 @@ for (i = 0; i < faq.length; i++) {
             body.style.display = "block";
         }
     });
+}
+
+*/
+
+
+function myFunction() {
+    // document.getElementById('changeview').style.display='none';
+    //document.getSelectorAll(".chang_view_but").style.display='none';
+    if (document.getElementById("shop2").className = "inshop1")
+    {
+        console.log("Hello motherfather.");
+        // document.getElementById("shop2").classList.remove("inshop")
+        document.getElementById("shop2").classList.replace("inshop1","inshop");
+        // document.getElementById("shop2").classList.toggle("inshop2")
+
+        var elemen1 = document.querySelectorAll('[id^="nvm"]');
+        console.log(elemen1);
+
+        var index = 0, length = elemen1.length;
+        for ( ; index  < length; index++) {
+            elemen1[index].classList.replace("thing_to_buy1","thing_to_buy");
+        }
+    }
+    if (document.getElementById("shop2").className = "inshop2")
+    {
+        console.log("Hello motherfather.");
+        // document.getElementById("shop2").classList.remove("inshop")
+        document.getElementById("shop2").classList.replace("inshop2","inshop");
+        // document.getElementById("shop2").classList.toggle("inshop2")
+
+        var elemen1 = document.querySelectorAll('[id^="nvm"]');
+        console.log(elemen1);
+
+        var index = 0, length = elemen1.length;
+        for ( ; index  < length; index++) {
+            elemen1[index].classList.replace("thing_to_buy2","thing_to_buy");
+        }
+    }
+
+}
+
+function myFunction1() {
+    // document.getElementById('changeview').style.display='none';
+    //document.getSelectorAll(".chang_view_but").style.display='none';
+    if (document.getElementById("shop2").className = "inshop")
+    {
+        // document.getElementById("shop2").classList.remove("inshop")
+        document.getElementById("shop2").classList.replace("inshop","inshop1");
+        // document.getElementById("shop2").classList.toggle("inshop");
+        // document.getElementById("shop2").classList.toggle("inshop2");
+        var elemen1 = document.querySelectorAll('[id^="nvm"]');
+        var index = 0, length = elemen1.length;
+        for ( ; index  < length; index++) {
+            elemen1[index].classList.replace("thing_to_buy","thing_to_buy1");
+        }
+
+    }
+    if (document.getElementById("shop2").className = "inshop2")
+    {
+        // document.getElementById("shop2").classList.remove("inshop")
+        document.getElementById("shop2").classList.replace("inshop2","inshop1");
+        // document.getElementById("shop2").classList.toggle("inshop");
+        // document.getElementById("shop2").classList.toggle("inshop2");
+        var elemen1 = document.querySelectorAll('[id^="nvm"]');
+        var index = 0, length = elemen1.length;
+        for ( ; index  < length; index++) {
+            elemen1[index].classList.replace("thing_to_buy2","thing_to_buy1");
+        }
+
+    }
+}
+
+
+
+function myFunction2() {
+    // document.getElementById('changeview').style.display='none';
+    //document.getSelectorAll(".chang_view_but").style.display='none';
+    if (document.getElementById("shop2").className = "inshop2")
+    {
+        console.log("Hello motherfather.");
+        // document.getElementById("shop2").classList.remove("inshop")
+        document.getElementById("shop2").classList.replace("inshop1","inshop2");
+        // document.getElementById("shop2").classList.toggle("inshop2")
+
+        var elemen1 = document.querySelectorAll('[id^="nvm"]');
+        console.log(elemen1);
+
+        var index = 0, length = elemen1.length;
+        for ( ; index  < length; index++) {
+            elemen1[index].classList.replace("thing_to_buy1","thing_to_buy2");
+        }
+
+    }
+    if (document.getElementById("shop2").className = "inshop")
+    {
+        console.log("Hello motherfather.");
+        // document.getElementById("shop2").classList.remove("inshop")
+        document.getElementById("shop2").classList.replace("inshop","inshop2");
+        // document.getElementById("shop2").classList.toggle("inshop2")
+
+        var elemen1 = document.querySelectorAll('[id^="nvm"]');
+        console.log(elemen1);
+
+        var index = 0, length = elemen1.length;
+        for ( ; index  < length; index++) {
+            elemen1[index].classList.replace("thing_to_buy","thing_to_buy2");
+        }
+
+    }
 }
